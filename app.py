d12b66e2b75ae9afea13ea3bb8f779c8a244b2f9
Precell <82501476+Precell@users.noreply.github.com>
2022-05-09 12:42:34 +0200

    checked

--- app.py
+++ app.py
@@ -13,6 +13,5 @@ BASE_URL = '/events/api'
 api.add_resource(EventsList, f'{BASE_URL}/Events')
 api.add_resource(Event, f'{BASE_URL}/Events/<event_id>')
 api.add_resource(ReviewList, f'{BASE_URL}/reviews/<event_id>')
-
 if __name__ == '__main__':
     app.run(debug=True)
