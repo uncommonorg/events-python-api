6709008075f0d9b9e4fcbd722b8faad71c190df0
Precell <82501476+Precell@users.noreply.github.com>
2022-05-12 15:46:15 +0200

    fixeing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from routes import EventsList, Event, UserList
+from routes import EventsList, Event, UserList, User
 
 app = Flask(__name__)
 CORS(app)
@@ -10,9 +10,10 @@ api = Api(app)
 
 BASE_URL = '/events/api'
 
-api.add_resource(EventsList, f'{BASE_URL}/events')
-api.add_resource(Event, f'{BASE_URL}/events/<event_id>')
-api.add_resource(UserList, f'{BASE_URL}/reviews/<event_id>')
+api.add_resource(EventsList, f'{BASE_URL}/Events')
+api.add_resource(Event, f'{BASE_URL}/Events/<event_id>')
+api.add_resource(UserList, f'{BASE_URL}/Users')
+api.add_resource(User, f'{BASE_URL}/Users/<event_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
