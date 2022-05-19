7fe8c7d5909ca0a8cd01a80c02faf28fc3084992
Precell <82501476+Precell@users.noreply.github.com>
2022-05-19 11:30:32 +0200

    pooling

--- app.py
+++ app.py
@@ -22,20 +22,21 @@ DB_PORT = os.environ.get("DB_PORT")
 DATABASE = os.environ.get("DATABASE")
 
 app.config['pSQL_pool'] = pool.SimpleConnectionPool(MIN,
-                                                    MAX,
-                                                    user = USER,
-                                                    host = HOST,
-                                                    database = DATABASE,
-                                                    password = PASSWORD,
-                                                    port = DB_PORT)
+                                            MAX,
+                                            user = USER,
+                                            host = HOST,
+                                            database = DATABASE,
+                                            password = PASSWORD,
+                                            port = DB_PORT)
 
 api.add_resource(EventsList, f'{BASE_URL}/Events')
 api.add_resource(Event, f'{BASE_URL}/Events/<event_id>')
 api.add_resource(UserList, f'{BASE_URL}/Users')
 api.add_resource(User, f'{BASE_URL}/Users/<event_id>')
+print(app.config['pSQL_pool'])
 
 @app.teardown_appcontext
-def close_conn():
+def close_conn(e):
     db = g.pop('db', None)
     if db is not None:
         app.config['pSQL_pool'].putconn(db)
